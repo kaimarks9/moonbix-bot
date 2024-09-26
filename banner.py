24dc850979db82e12c29553916ee794ce8020191
kai <123796253+kaimarks9@users.noreply.github.com>
2024-09-27 04:38:51 +0700

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,23 @@
+from colorama import *
+
+red = Fore.LIGHTRED_EX
+blue = Fore.LIGHTBLUE_EX
+cyan = Fore.LIGHTCYAN_EX
+green = Fore.LIGHTGREEN_EX
+manta = Fore.LIGHTMAGENTA_EX
+black = Fore.LIGHTBLACK_EX
+yellow = Fore.LIGHTYELLOW_EX
+white = Fore.LIGHTWHITE_EX
+magenta = Fore.LIGHTMAGENTA_EX
+reset = Style.RESET_ALL
+
+banner = f"""
+
+{blue} _  __     _   ____            _           _   
+{blue}| |/ /__ _(_) |  _ \ _ __ ___ (_) ___  ___| |_  {blue}Author   : kaimarks9 / {green}MoonBix!
+{blue}| ' // _` | | | |_) | '__/ _ \| |/ _ \/ __| __| {green}Github   : @kaimarks9
+{blue}| . \ (_| | | |  __/| | | (_) | |  __/ (__| |_  {yellow}Notes    : your doing is your respondbility!
+{blue}|_|\_\__,_|_| |_|   |_|  \___// |\___|\___|\__| {red}Warning  : this is for education purpose only!
+{blue}                            |__/
+
+        """
