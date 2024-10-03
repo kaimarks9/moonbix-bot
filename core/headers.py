185106af57b8325853320a3b436fdb56041190bc
kai <123796253+kaimarks9@users.noreply.github.com>
2024-10-03 07:57:11 +0700

    Update headers.py

--- core/headers.py
+++ core/headers.py
@@ -1,9 +1,11 @@
+from fake_useragent import UserAgent
+
 def headers(token=None):
     headers = {
         "Accept": "application/json, text/plain, */*",
         "Origin": "https://www.binance.info",
         "Referer": "https://www.binance.info/en/game/tg/moon-bix",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+        "User-Agent": UserAgent().random,
     }
     if token:
         headers["X-Growth-Token"] = token
